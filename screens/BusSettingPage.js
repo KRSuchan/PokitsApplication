350f13ddb30dddd83ac965a00bf93dbef0b264bc
Jinnouchi4796 <lhyyyy2@yahoo.com>
2023-09-25 21:45:29 +0900

    선호정류장 실제작동 _ AsyncStorage 성공

--- screens/BusSettingPage.js
+++ screens/BusSettingPage.js
@@ -1,8 +1,12 @@
-import React from 'react';
+//설정 조회를 위해 필요함
+import React, { useState, useEffect } from 'react';
+//설정 저장을 위해 필요함
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 import { StyleSheet, View, Text, Button, StyleSheetm, TouchableOpacity, Image } from 'react-native';
 import { Divider } from 'react-native-elements';
 
-const CheckItemBig = ({ title, onPress, describe}) => (
+const CheckItemBig = ({ title, onPress, describe, selected}) => (
     <TouchableOpacity style={{width:'100%'}} onPress={onPress}>
         <View style={styles.itemcontainer}>
             <View style-={styles.vbox}>
@@ -15,10 +19,11 @@ const CheckItemBig = ({ title, onPress, describe}) => (
                             {describe}
                         </Text>
                     </View>
-                    <Image 
+                    {selected && <Image 
             source={require('../assets/images/Done.png')} // 여기에 실제 이미지 경로 입력
             style={{ width: 30, height: 30 }} // 텍스트 높이만큼 이미지 크기 설정
-          />
+          />}
+                    
                 </View>
                 <Divider style={styles.dividerstyle} orientation="horizontal" />
             </View>
@@ -28,18 +33,48 @@ const CheckItemBig = ({ title, onPress, describe}) => (
   );
 
 export default function BusSettingPage({navigation}) {
+    const [selectedItem, setSelectedItem] = useState(null);
+
+    //설정 불러오기
+    useEffect(() => {
+        const loadSettings = async() => {
+            try{
+                //세팅을 변수에 담음
+                const savedSetting = await AsyncStorage.getItem('busSetting');
+                //비어있지 않다면 state 에 넣을 것임
+                if(savedSetting !== null) setSelectedItem(savedSetting);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+
+        loadSettings(); //이걸 해줘야 실제로 작동한다.
+    }, []);
+
+    //설정 저장하기
+    const saveSetting = async(setting) => {
+        try{
+            await AsyncStorage.setItem('busSetting',setting);
+            setSelectedItem(setting); //저장후 바로 state에 넣을 것임
+        } catch(error){
+            console.error(error);
+        }
+    };
+
     return(
         <View style={styles.container}>
             <Text style={styles.h1}>선호 정류장</Text>
             <CheckItemBig
                 title="옥계중학교 방면"
                 describe="옥계, 산동, 인동 방향"
-                onPress
+                onPress={() => saveSetting("옥계중학교방면")}
+                selected = {selectedItem === "옥계중학교방면"}
             />
             <CheckItemBig
                 title="구미시내 방면"
                 describe="구미역, 버스터미널, 사곡 방향"
-                onPress
+                onPress={()=> saveSetting("구미시내방면")}
+                selected = {selectedItem === "구미시내방면"}
             />
         </View>
     )
