78dd67885b4b2c00f34fbe329f8cf1d47245c1e7
Jinnouchi4796 <lhyyyy2@yahoo.com>
2023-10-07 18:10:05 +0900

    디데이 추가 작업중

--- screens/DdaySettingPage.js
+++ screens/DdaySettingPage.js
@@ -6,13 +6,41 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { StyleSheet, View, Text, Button, StyleSheetm, TouchableOpacity, Image } from 'react-native';
 import { Divider } from 'react-native-elements';
 
+// Setting item component
+const DdayItemBig = ({ title, onPress, describe}) => (
+    <TouchableOpacity style={{width:'100%'}} onPress={onPress}>
+        <View style={styles.itemcontainer}>
+            <View style-={styles.vbox}>
+                <View style={styles.hbox}>
+                    <View style={styles.vbox}>
+                        <Text style={styles.itemtitle}>
+                            {title}
+                        </Text>
+                        <Text style={styles.itemdescribe}>
+                            {describe}
+                        </Text>
+                    </View>
+                    <Image 
+            source={require('../assets/images/Next.png')} // 여기에 실제 이미지 경로 입력
+            style={{ width: 10, height: 30 }} // 텍스트 높이만큼 이미지 크기 설정
+          />
+                </View>
+                <Divider style={styles.dividerstyle} orientation="horizontal" />
+            </View>
+        </View>
+    </TouchableOpacity>
+    
+  );
+
 export default function DdaySettingPage({navigation}){
 
     return(
         <View style={styles.container}>
             <Text style={styles.h1}>디데이 설정</Text>
-            
-    
+            <DdayItemBig
+                title="디데이 추가"
+                describe="나만의 디데이를 추가해 보세요"
+            />
         </View>
     )
 
