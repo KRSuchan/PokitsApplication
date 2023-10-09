47f604166e09ab76f57af7f582878899e6d46caf
Jinnouchi4796 <lhyyyy2@yahoo.com>
2023-10-10 02:39:36 +0900

    약관작성완료 + html형태

--- screens/TermsPage.js
+++ screens/TermsPage.js
@@ -1,12 +1,18 @@
 //설정 조회를 위해 필요함
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, Text, Button, StyleSheetm, TouchableOpacity, Image } from 'react-native';
+import { StyleSheet, View, Text, Button, StyleSheetm, TouchableOpacity, Image, ScrollView } from 'react-native';
 import { Divider } from 'react-native-elements';
 
+import { WebView } from 'react-native-webview';
+
 export default function TermsPage ({navigation}) {
+
     return(
         <View style={styles.container}>
             <Text style={styles.h1}>이용약관</Text>
+                <WebView
+                    source={require('../assets/data/terms.html')}
+                    />
         </View>
     )
 }
@@ -18,6 +24,11 @@ const styles = StyleSheet.create({
       alignItems: 'flex-start',
       justifyContent: 'flex-start', 
       padding: 20,
+    },
+
+    scrollbox:{
+        flex:1,
+        width:'100%',
     },
 
     secondcontaioner: {
