d2f2cef4c7639f50bacc27810007e2df6615aab6
Jinnouchi4796 <lhyyyy2@yahoo.com>
2023-11-02 02:49:54 +0900

    버스 상단부분 중간저장

--- screens/BusPage.js
+++ screens/BusPage.js
@@ -2,10 +2,17 @@ import React from 'react';
 import { StyleSheet, View, Text, Button, StyleSheetm, TouchableOpacity, Image } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { Divider } from 'react-native-elements';
+import { LinearGradient } from "expo-linear-gradient";
+
 
 export default function BusPage({navigation}){
     return(
         <View style={styles.fullcontainer}> 
+            <LinearGradient colors={['#018242', '#00D26A']} start={{ x: 0, y: 0 }} end={{ x: 1, y: 0 }} style={styles.topView}>
+                <Text style={styles.h1}>
+                    Pokit's
+                </Text>
+            </LinearGradient>
             <View style={styles.topView}>
                 
             </View>
@@ -18,19 +25,23 @@ export default function BusPage({navigation}){
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
+    //   flex: 1,
       backgroundColor: '#fff',
       alignItems: 'flex-start',
       justifyContent: 'flex-start', 
       padding: 20,
     },
     topView:{
-        flex: 1,
-        height: 50,
+        // flex: 1,꽉채우면 안됨 컨테이너도 1이니까
         padding: 20,
     },
     fullcontainer:{
-        flex: 1, //화면 꽉채워줭
+        flex: 1, //화면 꽉채워줭, 
         backgroundColor: '#fff',
-    }
+    },
+    h1: {
+        fontFamily: "Lobster",
+        fontSize: 40,
+        color: "#fff",
+      },
 })
