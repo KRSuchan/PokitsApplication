f36a3ed2db5e500785a2dea7cf3ecb8db449e1d4
Jinnouchi4796 <lhyyyy2@yahoo.com>
2023-11-05 03:27:32 +0900

    시간추가, 로고이슈 해결

--- screens/MainPage.js
+++ screens/MainPage.js
@@ -189,6 +189,7 @@ export default function MainPage({ navigation }) {
                 resizeMode="cover"
                 shouldPlay
                 isLooping
+                autoPlay
             />
             </TouchableOpacity>
           </View>
